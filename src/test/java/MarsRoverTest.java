70e33bf99fda06a21f2c64b4c30da8bbad1abdd0
oocl-wonged7 <edmond.c.m.wong@oocl.com>
2024-11-19 16:19:46 +0800

    test: Add remaining test cases

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -24,6 +24,39 @@ class MarsRoverTest {
     }
 
     @Test
+    void should_face_south_when_turned_right_from_east() {
+        MarsRover rover = new MarsRover(0,0,"E");
+
+        rover.executeCommand("R");
+
+        String Status = rover.getStatus();
+
+        assertEquals("0:0:S", Status);
+    }
+
+    @Test
+    void should_face_west_when_turned_right_from_south() {
+        MarsRover rover = new MarsRover(0,0,"S");
+
+        rover.executeCommand("R");
+
+        String Status = rover.getStatus();
+
+        assertEquals("0:0:W", Status);
+    }
+
+    @Test
+    void should_face_north_when_turned_right_from_west() {
+        MarsRover rover = new MarsRover(0,0,"W");
+
+        rover.executeCommand("R");
+
+        String Status = rover.getStatus();
+
+        assertEquals("0:0:N", Status);
+    }
+
+    @Test
     void should_face_west_when_turned_left_from_north() {
         MarsRover rover = new MarsRover();
 
@@ -35,6 +68,39 @@ class MarsRoverTest {
     }
 
     @Test
+    void should_face_north_when_turned_left_from_east() {
+        MarsRover rover = new MarsRover(0,0,"E");
+
+        rover.executeCommand("L");
+
+        String Status = rover.getStatus();
+
+        assertEquals("0:0:N", Status);
+    }
+
+    @Test
+    void should_face_east_when_turned_left_from_south() {
+        MarsRover rover = new MarsRover(0,0,"S");
+
+        rover.executeCommand("L");
+
+        String Status = rover.getStatus();
+
+        assertEquals("0:0:E", Status);
+    }
+
+    @Test
+    void should_face_south_when_turned_left_from_west() {
+        MarsRover rover = new MarsRover(0,0,"W");
+
+        rover.executeCommand("L");
+
+        String Status = rover.getStatus();
+
+        assertEquals("0:0:S", Status);
+    }
+
+    @Test
     void should_move_forward_when_facing_north() {
         MarsRover rover = new MarsRover();
 
@@ -43,5 +109,42 @@ class MarsRoverTest {
         String Status = rover.getStatus();
 
         assertEquals("0:1:N", Status);
+    }
+
+    @Test
+    void should_move_forward_when_facing_east() {
+        MarsRover rover = new MarsRover();
+
+        rover.executeCommand("R");
+        rover.executeCommand("M");
+
+        String Status = rover.getStatus();
+
+        assertEquals("1:0:E", Status);
+    }
+
+    @Test
+    void should_move_forward_when_facing_south() {
+        MarsRover rover = new MarsRover();
+
+        rover.executeCommand("R");
+        rover.executeCommand("R");
+        rover.executeCommand("M");
+
+        String Status = rover.getStatus();
+
+        assertEquals("0:-1:S", Status);
+    }
+
+    @Test
+    void should_move_forward_when_facing_west() {
+        MarsRover rover = new MarsRover();
+
+        rover.executeCommand("L");
+        rover.executeCommand("M");
+
+        String Status = rover.getStatus();
+
+        assertEquals("-1:0:W", Status);
     }
 }
